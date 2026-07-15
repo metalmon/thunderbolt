220183d118eef12e9224a12590e9d2f6ce214732
metalmon <propiska63@gmail.com>
2026-07-15 23:34:39 +0400

    feat(i18n): one-shot ui_language detect on onboarding/preferences

---
+++ src/i18n/apply-initial-ui-language.ts
@@ -0,0 +1,22 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+import { setUiLanguage } from './i18n'
+import { readClientLocale, resolveInitialUiLanguage } from './ensure-ui-language'
+
+export const applyInitialUiLanguageIfNeeded = async (input: {
+  stored: string | null | undefined
+  setValue: (value: string) => Promise<unknown>
+}): Promise<void> => {
+  const { language, shouldPersist } = resolveInitialUiLanguage({
+    stored: input.stored,
+    locale: readClientLocale(),
+  })
+  if (!shouldPersist) {
+    setUiLanguage(language)
+    return
+  }
+  await input.setValue(language)
+  setUiLanguage(language)
+}
