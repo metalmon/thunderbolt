25c285ef212e6fdd8ae3ce25ebd9a39b2c0b8e03
metalmon <propiska63@gmail.com>
2026-07-16 00:02:52 +0400

    feat(i18n): translate built-in defaults at render time

---
+++ src/i18n/builtin-default-ids.ts
@@ -0,0 +1,14 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+export const BUILTIN_MODE_IDS = new Set(['mode-chat', 'mode-search', 'mode-research'])
+export const BUILTIN_SKILL_IDS = new Set([
+  '01996330-0000-7000-8000-000000000001',
+  '01996330-0000-7000-8000-000000000002',
+])
+export const BUILTIN_TASK_IDS = new Set([
+  '0198ecc5-cc2b-735b-b478-93f8db7202ce',
+  '0198ecc5-cc2b-735b-b478-96071aa92f62',
+  '0198ecc5-cc2b-735b-b478-99e9874d61ba',
+])
