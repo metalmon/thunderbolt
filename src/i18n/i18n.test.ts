8e4a46842269de14b8eb9810d1e0062a2aef5c84
metalmon <propiska63@gmail.com>
2026-07-20 00:10:08 +0400

    feat(i18n): seed initial UI language from OS locale for pre-auth screens

--- src/i18n/i18n.test.ts
+++ src/i18n/i18n.test.ts
@@ -2,8 +2,9 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-import { describe, expect, test } from 'bun:test'
+import { describe, expect, it, test } from 'bun:test'
 import i18n, { setUiLanguage } from './i18n'
+import { detectUiLanguage } from './languages'
 
 describe('i18n init', () => {
   test('loads english common strings', () => {
@@ -15,5 +16,15 @@ describe('i18n init', () => {
     setUiLanguage('ru')
     expect(i18n.t('settings:localization.languageLabel')).toBe('Язык')
     setUiLanguage('en')
+  })
+})
+
+describe('initial language from OS locale', () => {
+  it('selects ru for a Russian OS locale', () => {
+    expect(detectUiLanguage('ru-RU')).toBe('ru')
+  })
+  it('falls back to en for non-Russian locales', () => {
+    expect(detectUiLanguage('en-US')).toBe('en')
+    expect(detectUiLanguage(undefined)).toBe('en')
   })
 })
